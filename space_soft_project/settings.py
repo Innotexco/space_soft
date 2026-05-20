d613a22edfe65654a046eb6e11140bfcd56af781
futuregodwin <godwinfuture067@gmail.com>
2026-05-20 16:52:25 +0100

    static

--- space_soft_project/settings.py
+++ space_soft_project/settings.py
@@ -72,7 +72,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'space_soft_project.urls'
