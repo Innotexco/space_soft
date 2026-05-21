eb37e8c0c37ff823baecf5d6ba3bc69d44fc0a7b
futuregodwin <godwinfuture067@gmail.com>
2026-05-21 12:11:48 +0100

    Remove static_root from git tracking

--- space_soft_project/settings.py
+++ space_soft_project/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static_root"),
+    # os.path.join(BASE_DIR, "static_root"),
     os.path.join(BASE_DIR, "theme", "static"),
 ]
 
